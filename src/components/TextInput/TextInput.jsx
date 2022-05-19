586e1cb7212b98b461e8d7452c48298723bd73b2
Walter Velardo <waltervelardo@gmail.com>
2022-05-19 10:42:30 +0200

    finish

--- src/components/TextInput/TextInput.jsx
+++ src/components/TextInput/TextInput.jsx
@@ -1,7 +1,5 @@
 import { TextInput as NativeTextInput, StyleSheet } from "react-native";
 
-const styles = StyleSheet.create({});
-
 const TextInput = ({ style, error, ...props }) => {
   const textInputStyle = [style];
 
